88d8e64d1c242ba2f89a14647e523e17cc6f6815
Piyush Nishad <100183401+imarksea@users.noreply.github.com>
2025-01-29 12:06:30 +0530

    Fix: Font Style webpack.prod.js
    Signed-off-by: Piyush Nishad <100183401+imarksea@users.noreply.github.com>

--- webpack.prod.js
+++ webpack.prod.js
@@ -59,6 +59,13 @@ module.exports = {
           filename: "static/media/[name][ext]",
         },
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/fonts/[name][ext]",
+        },
+      },
     ],
   },
   optimization: {
